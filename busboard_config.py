f4a0adff18f9abdadb1262accb677058f3f59f07
Lance Erickson <lancetarn@gmail.com>
2014-12-27 09:20:27 -0600

    Cleanup

--- busboard_config.py
+++
@@ -1,7 +0,0 @@
-DATABASE = 'busboard'
-DEBUG = True
-SECRET_KEY = 'blahblahblahblah'
-USERNAME = 'rider'
-PASSWORD = '22 all the way home'
-NEXTRIP_XML_PREFIX = '{http://schemas.datacontract.org/2004/07/MetCouncil.Transit.ScheduleData}'
-NEXTRIP_BASE_URL = 'http://svc.metrotransit.org/NexTrip/'
